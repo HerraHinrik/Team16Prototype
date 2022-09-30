a585672e6155c584c417e5751f8e1f1de0f5ea96
SamanthaDavis2905 <samanthadavis2905@gmail.com>
2022-09-30 16:03:00 +0200

    Added player movement

---
+++ Team16Prototype/Assets/Scripts/characterMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    private bool canMove = true;
+    public float healthPoints = 100f;
+    public bool isDead = false;
+    private Vector3 playerVelocity;
+    public Transform cam;
+    private Vector3 direction;
+    public CharacterController controller;
+    public float playerSpeed = 6f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets input and makes you move around
+        if (canMove)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            direction = new Vector3(horizontal, 0f, vertical).normalized;
+        }
+
+        //smooths out turning
+        if (direction.magnitude >= 0.1f && canMove)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * playerSpeed * Time.deltaTime);
+
+        }
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+
+}
