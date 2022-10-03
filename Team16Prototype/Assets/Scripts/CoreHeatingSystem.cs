5221c8470cb7a836d942c0f31e178e73d49a3c28
HerraHinrik <60097312+HerraHinrik@users.noreply.github.com>
2022-10-03 16:18:51 +0200

    Core Heating not starts to fill up if player presses v

--- Team16Prototype/Assets/Scripts/CoreHeatingSystem.cs
+++ Team16Prototype/Assets/Scripts/CoreHeatingSystem.cs
@@ -2,25 +2,43 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoreHeatingSystem : MonoBehaviour
 {
     [SerializeField]public bool playerMovement;
-
     [SerializeField] private float maxCoreHealth;
-
     [SerializeField] private float currentCoreHealth;
+    [SerializeField] private Image coreSlider;
 
+    private void Start()
+    {
+        currentCoreHealth = 0f;
+        playerMovement = false;
+    }
     private void Update()
     {
-        currentCoreHealth += Time.deltaTime;
-    }
 
-    private void Whatiscoreat()
-    {
+        if (playerMovement == true)
+        {
+            currentCoreHealth += Time.deltaTime;
+            SetSlider();
+                    if (currentCoreHealth >= maxCoreHealth)
+                    {
+                        Debug.Log("die");
+                        Destroy(gameObject);
+                        
+                    }
+        }
+
         if (Input.GetKeyDown(KeyCode.V))
         {
-            Debug.Log("Core : " + currentCoreHealth.ToString());
+            playerMovement = true;
         }
+    }
+
+    public void SetSlider()
+    {
+        coreSlider.fillAmount = currentCoreHealth / maxCoreHealth;
     }
 }
