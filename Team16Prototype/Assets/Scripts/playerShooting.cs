827325033f8f4345ccdb976d15521ca3c9d96d93
Douglas <douglas.torn-persson@edu.futuregames.nu>
2022-10-05 18:15:46 +0200

    Shooting, EnemyHealth, Laser
    New branch since the last one broke

--- Team16Prototype/Assets/Scripts/playerShooting.cs
+++ Team16Prototype/Assets/Scripts/playerShooting.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class playerShooting : MonoBehaviour
 {
-    public GameObject projectilePrefab;
-    [SerializeField] private GameObject laser;
-    public bool canShoot = true;
-    public Transform shootingPosition;
-    public GameObject player;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // public GameObject projectilePrefab;
+    [SerializeField] private float range = 100f;
+    // public bool canShoot = true;
+    [SerializeField] public Transform shootingPosition;
+    // public GameObject player;
+    [SerializeField] private float damage = 20f;
+    [SerializeField] private ParticleSystem laserFlash;
 
     // Update is called once per frame
     void Update()
@@ -25,27 +20,36 @@ public class playerShooting : MonoBehaviour
             Instantiate(projectilePrefab, shootingPosition.position, player.transform.rotation);
             
         }*/
-        ProcessFiring();
+         if (Input.GetButtonDown("Fire1"))
+         {
+             Shoot();
+         }
+    }
+    
+    // ReSharper disable Unity.PerformanceAnalysis
+    void Shoot()
+    {
+        PlayLaserFlash();
+        ProcessRaycast();
     }
 
-    void ProcessFiring()
+    void PlayLaserFlash()
     {
-        if (Input.GetButton("Fire1") && canShoot)
+        laserFlash.Play();
+    }
+
+    void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
         {
-            SetLasersActive(true);
+            Debug.Log(hit.transform.name + " was hit");
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            target.TakeDamage(damage);
         }
         else
         {
-            SetLasersActive(false);
+            return; 
         }
-    }
-    
-    void SetLasersActive(bool isActive)
-    {
-        //activate laser
-        //laser.SetActive(true);
-
-        var emissionModule = laser.GetComponent<ParticleSystem>().emission;
-        emissionModule.enabled = isActive;
     }
 }
