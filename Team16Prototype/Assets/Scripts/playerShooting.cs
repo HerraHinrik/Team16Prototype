5593bdc3d193841581eebbcc474eef812e9be96a
Douglas <douglas.torn-persson@edu.futuregames.nu>
2022-10-05 21:32:44 +0200

    Added hit enemy effect

--- Team16Prototype/Assets/Scripts/playerShooting.cs
+++ Team16Prototype/Assets/Scripts/playerShooting.cs
@@ -11,6 +11,7 @@ public class playerShooting : MonoBehaviour
     // public GameObject player;
     [SerializeField] private float damage = 20f;
     [SerializeField] private ParticleSystem laserFlash;
+    [SerializeField] private GameObject hitEffect;
 
     // Update is called once per frame
     void Update()
@@ -44,12 +45,20 @@ public class playerShooting : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name + " was hit");
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
             target.TakeDamage(damage);
         }
         else
         {
             return; 
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.identity);
+        Destroy(impact, 1);
     }
 }
